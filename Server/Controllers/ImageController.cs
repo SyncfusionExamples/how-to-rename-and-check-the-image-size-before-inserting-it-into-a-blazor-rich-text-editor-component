7605bda498bf90f0a6f32dcc55a04f13700a8149
Arulraj A <arul.raj47@gmail.com>
2021-12-03 16:25:46 +0530

    Committed the example project.

---
+++ Server/Controllers/ImageController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace MyBlazorApp.Server.Controllers
+{
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private double x;
+        private string imageFileName;
+        private readonly IWebHostEnvironment hostingEnv;
+
+        public ImageController(IWebHostEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        [HttpPost("[action]")]
+        [Route("api/Image/Rename")]
+        public void Rename(IList<IFormFile> UploadFiles)
+        {
+            try
+            {
+                foreach (IFormFile file in UploadFiles)
+                {
+                    if (UploadFiles != null)
+                    {
+                        string targetPath = hostingEnv.ContentRootPath + "\\wwwroot\\Images";
+                        string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                        // Create a new directory, if it does not exists
+                        if (!Directory.Exists(targetPath))
+                        {
+                            Directory.CreateDirectory(targetPath);
+                        }
+
+                        imageFileName = filename;
+                        string path = hostingEnv.WebRootPath + "\\Images" + $@"\{filename}";
+
+                        // Rename a uploaded image file name
+                        while (System.IO.File.Exists(path))
+                        {
+                            imageFileName = "rteImage" + x + "-" + filename;
+                            path = hostingEnv.WebRootPath + "\\Images" + $@"\rteImage{x}-{filename}";
+                            x++;
+                        }
+
+                        if (!System.IO.File.Exists(path))
+                        {
+                            using (FileStream fs = System.IO.File.Create(path))
+                            {
+                                file.CopyTo(fs);
+                                fs.Flush();
+                                fs.Close();
+                            }
+
+                            // Modified file name shared through response header by adding custom header
+                            Response.Headers.Add("name", imageFileName);
+                            Response.StatusCode = 200;
+                            Response.ContentType = "application/json; charset=utf-8";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+            }
+        }
+    }
+}
